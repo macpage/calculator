2803124395a58f5ae082adf75c6468608c352f68
Mac <sophanpa@gmail.com>
2022-08-23 23:43:01 +0200

    Add add function

--- app.js
+++ app.js
@@ -13,3 +13,95 @@ function checkTime(i){
     }
     return i;
 }
+
+let opActive = false;
+
+let num = document.querySelector(".number"); 
+
+let ops = document.querySelectorAll(".operator");
+
+let op = "";
+
+for(let i = 0; i<ops.length;i++){
+    ops[i].addEventListener("click", () => {
+        arr.push(num.innerHTML);
+        
+        opActive = true;
+        if(op == "+"){
+            operate();
+        } else {
+           
+        }
+        
+        op =ops[i].innerHTML;
+        return op;
+        
+    })
+}
+
+let arr = [];
+arr[0] = 0;
+
+let lol = document.querySelectorAll(".numbers");
+
+for(let i = 0;i<lol.length;i++){
+    lol[i].addEventListener("click", function(){
+        
+        
+            if(num.innerHTML == 0){
+                num.innerHTML = "";
+            }
+            
+       
+          if(!opActive){
+            num.innerHTML += parseInt(lol[i].innerHTML,10); 
+          } else {
+            num.innerHTML = lol[i].innerHTML;
+            opActive = false
+           
+          }
+           
+        
+        
+        
+        if(op == "+"){
+            
+            
+            
+        }
+        else if(op == "-"){
+            subtract(parseInt(arr[arr.length-2],10),parseInt(arr[arr.length-1],10));
+        }
+    });
+    
+}
+
+let is = document.querySelector("#is");
+is.addEventListener("click",  operate)
+
+
+function operate(){
+    
+    if(op == "+"){
+         add(parseInt(num.innerHTML),parseInt(arr[arr.length-1],10));
+         return num.innerHTML =  arr[arr.length-1]
+    }
+}
+
+function add(num,num2){
+    arr.push(num+num2);
+   return num + num2;
+}
+
+function subtract(num,num2){
+    arr.push(num-num2)
+    return num - num2;
+}
+
+function multiply(num1,num2){
+
+}
+
+function divide(num1,num2){
+
+}
